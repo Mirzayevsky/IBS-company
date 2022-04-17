bd5fea8c31ab3748b9f12b4d2655984d752405aa
Sirojiddin <79570217+Mirzayevsky@users.noreply.github.com>
2022-04-17 13:15:20 +0500

    lesson 9 - 12

---
+++ lesson -11/index.js
@@ -0,0 +1,17 @@
+// let number = prompt("enter number: ")
+// let degree = prompt("enter degree: ")
+
+// Number(number,degree)
+
+// let power  = function(x,y) {
+//     let = result = 1;
+     
+//     for(i=0; i<y; i++) {
+//         result *= x;
+//     }
+//     return result;
+// }
+
+//  let mainResult = power(number,degree)
+//  console.log(mainResult);
+
